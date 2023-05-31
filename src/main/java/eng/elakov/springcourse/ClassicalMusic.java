03343dd0e647b1c9ce3cec1beb9ef72a4bbf568d
Nick <nelakov24@gmail.com>
2023-05-31 22:22:33 +0300

    lesson-8: SpringConfig

--- src/main/java/eng/elakov/springcourse/ClassicalMusic.java
+++ src/main/java/eng/elakov/springcourse/ClassicalMusic.java
@@ -1,13 +1,8 @@
 package eng.elakov.springcourse;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
 
     @PostConstruct
--- src/main/java/eng/elakov/springcourse/TestSpring.java
+++ src/main/java/eng/elakov/springcourse/TestSpring.java
@@ -1,11 +1,11 @@
 package eng.elakov.springcourse;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
