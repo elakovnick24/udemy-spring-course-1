fe0d5f9ccdfe0fe8cb5e09feae434212965c4baa
Nick <nelakov24@gmail.com>
2023-05-31 21:32:12 +0300

    lesson-6: Qualifier

--- src/main/java/eng/elakov/springcourse/MusicPlayer.java
+++ src/main/java/eng/elakov/springcourse/MusicPlayer.java
@@ -1,20 +1,22 @@
 package eng.elakov.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
 }
