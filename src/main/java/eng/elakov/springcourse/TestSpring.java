7f76d73176ee65d5a72e1974b6118934c622ec3c
Nick <nelakov24@gmail.com>
2023-05-31 21:10:46 +0300

    lesson-2: Dependency injection

--- src/main/java/eng/elakov/springcourse/TestSpring.java
+++ src/main/java/eng/elakov/springcourse/TestSpring.java
@@ -8,9 +8,10 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+        // Music music = context.getBean("musicBean", Music.class);
+        // MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
 
